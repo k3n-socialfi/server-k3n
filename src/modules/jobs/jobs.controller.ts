5ad187d686909f84c9f78fbcef4666bb4883bef4
Thanh Hoang <dinhthanh2000a@gmail.com>
2024-05-02 00:15:49 +0700

    chore: change get jobs detail url

--- src/modules/jobs/jobs.controller.ts
+++ src/modules/jobs/jobs.controller.ts
@@ -42,7 +42,7 @@ export class JobsController {
     };
   }
 
-  @Get(':jobId')
+  @Get(':jobId/detail')
   //   @ApiResponse({
   //     description: 'Get user by id response',
   //     type: SwaggerUserResponseDto
