82e75f21ee6664f62024b77e2080078feea4fc90
tungxuan <tungxuan@tungxuans-Mac-mini.local>
2024-06-13 02:14:19 +0700

    feat: shillscore new version

--- src/modules/twitter/twitter.service.ts
+++ src/modules/twitter/twitter.service.ts
@@ -37,9 +37,9 @@ export class TwitterService {
     @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: Logger
   ) {}
 
-  //@Cron('0 0 * * *')
-  // @Timeout(0)
+  @Cron('0 0 * * *')
   async KolsRankingJob() {
+  // @Timeout(0)
     try {
       console.log('Start update ranking job !');
       await this.updateRanks();
@@ -50,7 +50,7 @@ export class TwitterService {
   }
 
   // 7 day update
-  //@Cron('0 0 * * 0')
+  @Cron('0 0 * * 0')
   // @Timeout(0)
   async KolsRankingJob7DaysUpdate() {
     try {
@@ -62,7 +62,7 @@ export class TwitterService {
     }
   }
 
-  //@Cron('0 0 1 * *')
+  @Cron('0 0 1 * *')
   // @Timeout(0)
   async KolsRankingJob30DaysUpdate() {
     try {
@@ -79,18 +79,19 @@ export class TwitterService {
   async TwitterJob() {
     try {
       console.log('Start run update previous point !');
-      await this.KolsRankingJob();
-      await this.KolsRankingJob7DaysUpdate();
-      await this.KolsRankingJob30DaysUpdate();
+      //await this.KolsRankingJob();
+      //await this.KolsRankingJob7DaysUpdate();
+      //await this.KolsRankingJob30DaysUpdate();
 
       console.log('Start run twitter job !');
 
       // Get points
-      await this.twitterPointsCalculation();
+      //await this.twitterPointsCalculation();
+      await this.twitterPointsCalculationNewVersion();
       // await this.twitterPointsCalculationByUsername('DustinH_13');
 
       // Create User's Portfolio
-      await this.createUserTwitterPortfolio();
+      //await this.createUserTwitterPortfolio();
 
       // const batchSize = 1;
       // const batches = [];
@@ -984,5 +985,48 @@ export class TwitterService {
     const year = dateParts[5];
 
     return `${day}-${month}-${year}`;
+  }
+
+  async twitterPointsCalculationNewVersion() {
+    const twitterUsers = await this.twitterUsersRep.find();
+    for (let i = 0; i < twitterUsers.length; i++) {
+      const twPoints = await this.getUserTweetPoints({ username: twitterUsers[i].username });
+
+      let view = twPoints.latestTweet.views;
+      let like = twPoints.latestTweet.favoriteCount;
+      let retweet = twPoints.latestTweet.retweetCount;
+      let reply = twPoints.latestTweet.replyCount;
+      // get user portfolio
+      const userPortfolio = await this.getUserTwitterPortfolio(twitterUsers[i].username);
+
+      const shillScoresList: number[] = [];
+
+      for (let k = 0; k < userPortfolio.length; k++){
+        const shillScore = await this.calculateShillScoreNewVersion(view, like, retweet, reply, userPortfolio[k].ath, userPortfolio[k].currentPrice);
+        shillScoresList.push(shillScore);
+      }
+
+      // calculate average shill score
+      const total = shillScoresList.reduce((sum, score) => sum + score, 0);
+      let finalScore = Math.floor(total / shillScoresList.length);
+
+      if (finalScore > 9999) finalScore = 9999;
+      if (finalScore < 1) finalScore = 1;
+      console.log('savePoint:', finalScore);
+
+      twitterUsers[i].twitterPoints = finalScore;
+
+      await this.twitterUsersRep.save(twitterUsers[i]);
+    }
+  }
+
+  async calculateShillScoreNewVersion(view: number, like: number, retweet: number, reply: number, ath: number, currentPrice: number) {
+    // Calculate the weighted raw score
+    const raw = (view + 2 * like + 4 * (retweet + reply)) / 11 + (3 * ath + currentPrice) / 4;
+
+    // Calculate the shill score
+    const shill = 9998 * raw + 1;
+
+    return shill;
   }
 }
